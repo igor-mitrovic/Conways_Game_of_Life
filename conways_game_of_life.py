7869bfe4cc609173bbe2a264ed4569f0f2e1ce6f
Igor Mitrovic <igor.mit@hotmail.com>
2024-03-20 18:58:39 +0100

    add commit again due to previous faliure

--- conways_game_of_life.py
+++ conways_game_of_life.py
@@ -1,8 +1,16 @@
 #Convays Game of Life in Python implemented in pygame
 import pygame
+import sys
 
 
 pygame.init() 
 #Initialize all of the imported pygame moduels
 
 root = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Conways Game of Life v1.0")
+
+flag = True
+while flag: #Set the main windows loop 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            flag = False
