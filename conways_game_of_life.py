610e5bae17be3f0a5fcd4c09000c1ee2f85fcfac
Igor Mitrovic <igor.mit@hotmail.com>
2024-03-23 23:43:56 +0100

    add cell and background colors

--- conways_game_of_life.py
+++ conways_game_of_life.py
@@ -1,6 +1,15 @@
 #Convays Game of Life in Python implemented in pygame
 import pygame
+import numpy as np
+import time
 import sys
+
+#Defining colors 
+
+COLOR_BACKGROUND = (10,10,10)
+COLOR_GRID = (40,40,40)
+COLOR_DIE_NEXT = (170,170,170)
+COLOR_ALIVE_NEXT = (255,255,255)
 
 
 pygame.init() 
@@ -9,8 +18,13 @@ pygame.init()
 root = pygame.display.set_mode((800,600))
 pygame.display.set_caption("Conways Game of Life v1.0")
 
+clock = pygame.time.Clock(60) #The game will run in 60 frames per second
+
 flag = True
 while flag: #Set the main windows loop 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             flag = False
+
+#Need to create conway logic
+#Split the screen into squares
