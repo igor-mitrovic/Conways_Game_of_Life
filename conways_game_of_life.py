ded3be128f956ad51e92156028a1429157ef7fbd
Igor Mitrovic <igor.mit@hotmail.com>
2024-03-24 02:46:34 +0100

    finish the basic game form

--- conways_game_of_life.py
+++ conways_game_of_life.py
@@ -11,20 +11,65 @@ COLOR_GRID = (40,40,40)
 COLOR_DIE_NEXT = (170,170,170)
 COLOR_ALIVE_NEXT = (255,255,255)
 
+def update(screen,cells,size,with_progress=False):
+    update_cells = np.empty((cells.shape[0],cells.shape[1]))
 
-pygame.init() 
-#Initialize all of the imported pygame moduels
+    for row, col in np.ndindex(cells.shape):
+        alive = np.sum(cells[row-1:row+2,col-1:col+2]) - cells[row,col]
+        color = COLOR_BACKGROUND if cells[row,col] == 0 else COLOR_ALIVE_NEXT
 
-root = pygame.display.set_mode((800,600))
-pygame.display.set_caption("Conways Game of Life v1.0")
+        if cells[row,col] == 1:
+            if alive < 2 or alive > 3 :
+                if with_progress == True:
+                    color = COLOR_DIE_NEXT
+            elif 2 <= alive <=3:
+                update_cells[row,col] = 1
+                if with_progress:
+                    color = COLOR_ALIVE_NEXT 
+        else:
+            if alive == 3:
+                update_cells[row,col] = 1
+                if with_progress:
+                    color = COLOR_ALIVE_NEXT
+        pygame.draw.rect(screen,color,(col*size,row*size,size-1,size-1))
+    return update_cells
 
-clock = pygame.time.Clock(60) #The game will run in 60 frames per second
 
-flag = True
-while flag: #Set the main windows loop 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            flag = False
+def main():
+    pygame.init() 
+    #Initialize all of the imported pygame moduels
 
-#Need to create conway logic
-#Split the screen into squares
+    root = pygame.display.set_mode((800,600))
+    pygame.display.set_caption("Conways Game of Life v1.0")
+    cells = np.zeros((60,80))
+    root.fill(COLOR_GRID)
+    update(root,cells,10)
+
+    pygame.display.flip()
+    pygame.display.update()
+    running = False
+    while True: #Set the main windows loop 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    running = not running
+                    update(root,cells,10)
+                    pygame.display.update()
+            if pygame.mouse.get_pressed()[0]:
+                pos = pygame.mouse.get_pos()
+                cells[pos[1] // 10, pos[0] // 10] = 1
+                update(root,cells,10)
+                pygame.display.update()
+
+        root.fill(COLOR_GRID)
+
+        if running:
+            cells = update(root,cells,10,with_progress=True)
+            pygame.display.update()
+        time.sleep(0.01)
+
+if __name__ == '__main__':
+    main()
